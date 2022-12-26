845645bdf5006fdf2c9661958ec9f2dde111cc55
WIN-CF9BAEAH6EV\user <zuev.f.s@gmail.com>
2022-12-26 13:35:48 +1000

    Import TextMeshPro Package

--- Assets/Scripts/Screen/MainScreenView.cs
+++ Assets/Scripts/Screen/MainScreenView.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainScreenView : MonoBehaviour
 {
     [SerializeField] private GameObject _scrollContent;
-    [SerializeField] private Text _balanceText;
+    [SerializeField] private TMP_Text _balanceText;
 
     private void Start()
     {
